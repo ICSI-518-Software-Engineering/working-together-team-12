e48f1653e8a005165fd539dacf4ae4b0040a6fcf
sai2615 <snarisetty@albany.edu>
2024-01-28 22:15:23 -0500

    Implementing multiplication function

--- calculator.js
+++ calculator.js
@@ -1,16 +1,16 @@
 // Multiplication
 function multiply(a, b) {
-    // return a * b;
+    return a * b;
 }
 
 // Addition
 function add(a, b) {
- return a + b;
+    // return a + b;
 }
 
 // Subtraction
 function subtract(a, b) {
-   // return a - b;
+    return a - b;
 }
 
 // Multiplication
@@ -21,9 +21,9 @@ function multiply(a, b) {
 // Division
 function divide(a, b) {
     if (b !== 0) {
-        // return a / b;
+         return a / b;
     } else {
-       // return "Cannot divide by zero";
+        return "Cannot divide by zero";
     }
 }
 
