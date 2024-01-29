f9ace948e4b9516bad67846eb3bc6fac82e67a4e
A Sai Narasimha Reddy <nrasimha20@gmail.com>
2024-01-29 11:46:27 -0800

    Implementing division function

--- calculator.js
+++ calculator.js
@@ -20,11 +20,11 @@ function multiply(a, b) {
 
 // Division
 function divide(a, b) {
-//     if (b !== 0) {
-//          return a / b;
-//     } else {
-//         return "Cannot divide by zero";
-//     }
+     if (b !== 0) {
+          return a / b;
+     } else {
+         return "Cannot divide by zero";
+     }
 }
 
 // Examples
