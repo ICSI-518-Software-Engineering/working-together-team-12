6c8a6a0b8dc3dd1ffad640941c26b72140fee11e
varshinirama123 <varshinichowdary939@gmail.com>
2024-04-27 17:12:22 -0400

    Login and register pages added

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
+    path('register',views.register,name='register'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
